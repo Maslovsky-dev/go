2212cca51be742fb11912b59a3ea5847fa2a1ef9
vmaslovskiy <vmaslovskiy@diasoft.ru>
2025-11-10 12:19:18 +0300

    Пустые строки

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,12 +11,9 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var builder strings.Builder
-
 	var isShielding = false
-
 	runes := []rune(s) //Приводим строку к массиву рун, чтобы обрабатывать все символы Unicode
 	for i := 0; i < len(runes); i++ {
-
 		if runes[i] == '\\' {
 			if i == len(runes)-1 {
 				return "", ErrInvalidString
@@ -26,7 +23,6 @@ func Unpack(s string) (string, error) {
 			i++
 			continue
 		}
-
 		if unicode.IsDigit(runes[i]) { //Проверка на число
 			if i == 0 {
 				return "", ErrInvalidString
@@ -34,24 +30,19 @@ func Unpack(s string) (string, error) {
 			if unicode.IsDigit(runes[i-1]) && !isShielding { //Два числа подряд, кроме случая с экранированием
 				return "", ErrInvalidString
 			}
-
 			num, _ := strconv.Atoi(string(runes[i]))
-
 			if num == 0 {
 				str := builder.String()
 				builder.Reset()
 				builder.WriteString(str[:len(str)-1])
 			}
-
 			if num > 1 {
 				builder.WriteString(strings.Repeat(string(runes[i-1]), num-1))
 			}
 			isShielding = false
 			continue
 		}
-
 		builder.WriteRune(runes[i])
 	}
-
 	return builder.String(), nil
 }
